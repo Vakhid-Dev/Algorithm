d7315734e2b5f11131a241617e9bd5715fab4b63
Unbroken <36956290+VakhidGuliev@users.noreply.github.com>
2020-08-15 11:16:23 +0300

    Update Implement.cs

--- src/Implement.cs
+++ src/Implement.cs
@@ -93,7 +93,13 @@
             a = b;
             b = temp;
         }
-
+        //Linq
+       public static bool IsPrime(int n)
+       {
+        if (n <= 2 || n % 2 == 0) return n == 2;
+        for (int i = 3; i <= Math.Sqrt(n); i += 2) if (n % i == 0) return false;
+        return true;
+      }
         public static bool IsPrime(int n)
         {
              if (n == 2)
